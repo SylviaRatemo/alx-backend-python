18dadd6c26725a0b04d89a95b5ca36a1d83490cb
Sylvia Ratemo <ratemosylvia@gmail.com>
2023-10-05 19:42:17 +0300

    task 11 issues

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -3,10 +3,9 @@
 '''
 from typing import TypeVar, Union, Any, Mapping
 T = TypeVar('T')
-default = Union[T, None]
 
 
-def safely_get_value(dct: Mapping, key: Any, default) -> Union[Any, T]:
+def safely_get_value(dct: Mapping, key: Any, default: Union[T, None]) -> Union[Any, T]:
     ''' more type annot
     '''
     if key in dct:
