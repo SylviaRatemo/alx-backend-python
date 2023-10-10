94d44953c7af67329e21d2e1443641a7aafef6de
Sylvia Ratemo <ratemosylvia@gmail.com>
2023-10-10 04:02:01 +0300

    asyncio syntax

---
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+'''Task 0
+'''
+import random
+import asyncio
+
+
+async def wait_random(max_delay: int = 1) -> float:
+    delay = random.uniform(0, max_delay)
+    await asyncio.sleep(delay)
+    return delay
